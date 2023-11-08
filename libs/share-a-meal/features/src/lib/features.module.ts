93b421613f6e993f9619a2774bbb5be7ec225592
Levi <gjj.kooy@student.avans.nl>
2023-11-08 12:02:13 +0100

    Angular front-end

--- libs/share-a-meal/features/src/lib/features.module.ts
+++ libs/share-a-meal/features/src/lib/features.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MealListComponent } from './meal/meal-list/meal-list.component';
+import { MealDetailComponent } from './meal/meal-detail/meal-detail.component';
+import { HttpClientModule } from '@angular/common/http';
+import { MealService } from './meal/meal.service';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, HttpClientModule],
+  declarations: [MealListComponent, MealDetailComponent],
+  providers: [MealService],
+  exports: [MealListComponent, MealDetailComponent],
 })
 export class FeaturesModule {}
